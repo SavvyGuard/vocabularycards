e46ca560adea869762ca9b312c3b931edad99bca
James Wu <james@jamesjwu.com>
2013-06-27 02:07:00 -0700

    remove all

--- src/James/VocabBundle/Resources/views/Main/about.html.php
+++
@@ -1,7 +0,0 @@
-<?php $view->extend('JamesVocabBundle::layout.html.php') ?>
-<?php $view['slots']->start('body') ?>
-
-	<h3> About Me</h3>	 
-	<p>	I'm a  programmer/analyst in Los Angeles. I do a lot of side projects for fun, mostly statistcs and modeling related.</p>   
-
-<?php $view['slots']->stop() ?>

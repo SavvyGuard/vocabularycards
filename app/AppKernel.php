da2bec8768ce00ac6128539b05af3cc81926c88c
James <james@jamesjwu.com>
2013-07-02 02:07:08 -0700

    Update AppKernel.php

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -10,7 +10,6 @@ class AppKernel extends Kernel
         $bundles = array(
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
             new Symfony\Bundle\SecurityBundle\SecurityBundle(),
-            new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),            new JamesWu\VocabBundle\JamesWuVocabBundle(),
         );
